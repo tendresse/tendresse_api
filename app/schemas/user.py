e1624d63c5000d812f71154f59e740ef9026a181
Tendresse <tendresseapp@gmail.com>
2016-07-29 00:21:14 +0200

    Update user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,12 +1,14 @@
+from marshmallow import fields
 from .. import ma
 from ..models.user import User
+from ..schemas.success import success_schema, successes_schema
 
 
 class UserSchema(ma.ModelSchema):
 
-    class Meta:
-        fields = ("id", "username", "achievements")
-        exclude = ("password", "friends", "devices")
+	id = fields.Int()
+	username = fields.String()
+	achievements = fields.Nested(successes_schema, many=True)
 
 
 user_schema = UserSchema()
